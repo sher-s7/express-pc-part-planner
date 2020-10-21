ca44b9cc8d7e57894f0dcb4c638ae98a548f1fad
Sher <sher.sheikh77@gmail.com>
2020-10-20 20:02:50 -0400

    Add node-localstorage

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,32 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+var async = require("async");
+
+var Category = require("../models/category");
+var ComputerPart = require("../models/computerpart");
+var Manufacturer = require("../models/manufacturer");
+
 // Require controller modules.
-var category_controller = require('../controllers/categoryController');
-var computerpart_controller = require('../controllers/computerpartController');
-var manufacturer_controller = require('../controllers/manufacturerController');
+var category_controller = require("../controllers/categoryController");
+var computerpart_controller = require("../controllers/computerpartController");
+var manufacturer_controller = require("../controllers/manufacturerController");
 
 /* GET home page. */
-router.get('/', category_controller.index);
+router.get("/", function (req, res) {
+  res.redirect("list");
+});
+
+router.get("/list", function (req, res, next) {
+  if (typeof localStorage === "undefined" || localStorage === null) {
+    console.log('hello')
+    var LocalStorage = require("node-localstorage").LocalStorage;
+    localStorage = new LocalStorage("./scratch");
+    localStorage.setItem("userList", JSON.stringify({ cpu: "none" }));
+  }
+  res.render("list", {
+    userList: localStorage.getItem("userList"),
+  });
+});
 
 module.exports = router;
