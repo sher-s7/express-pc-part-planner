663bdabedba4716773a74da6c00f37b1d8401471
Sher <sher.sheikh77@gmail.com>
2020-11-03 13:49:33 -0500

    Implement component deletion

--- controllers/computerpartController.js
+++ controllers/computerpartController.js
@@ -8,14 +8,17 @@ const { sanitizeBody, sanitize } = require("express-validator/filter");
 
 // Display list of all ComputerParts.
 exports.computerpart_list = function (req, res, next) {
-  ComputerPart.find().populate('manufacturer').populate('category').exec(function (err, list_computerparts) {
-    if (err) next(err);
+  ComputerPart.find()
+    .populate("manufacturer")
+    .populate("category")
+    .exec(function (err, list_computerparts) {
+      if (err) next(err);
 
-    res.render("component_list", {
-      title: "All Computer Parts",
-      component_list: list_computerparts,
+      res.render("component_list", {
+        title: "All Computer Parts",
+        component_list: list_computerparts,
+      });
     });
-  });
 };
 
 // Display detail page for a specific ComputerPart.
@@ -65,8 +68,11 @@ exports.computerpart_create_post = [
     .isLength({ min: 3 })
     .escape(),
   body("description").trim().escape(),
-  body("inStock", "Stock cannot be lower than 0").isInt({min: 0, max: 9999}),
-  body("price", "Price must be between $0 and $999999").isFloat({min: 0, max: 999999}),
+  body("inStock", "Stock cannot be lower than 0").isInt({ min: 0, max: 9999 }),
+  body("price", "Price must be between $0 and $999999").isFloat({
+    min: 0,
+    max: 999999,
+  }),
   body("category", "Category must not be empty").trim().escape(),
   body("manufacturer", "Manufacturer must not be empty").trim().escape(),
 
@@ -115,13 +121,33 @@ exports.computerpart_create_post = [
 ];
 
 // Display ComputerPart delete form on GET.
-exports.computerpart_delete_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: ComputerPart delete GET");
+exports.computerpart_delete_get = function (req, res, next) {
+  ComputerPart.findById(req.params.id)
+    .populate("category")
+    .populate("manufacturer")
+    .exec(function (err, component) {
+      if (err) next(err);
+
+      if (component == null) {
+        res.redirect("/components");
+      }
+
+      res.render("component_delete", {
+        title: "Delete Component: " + component.name,
+        component: component,
+        category: component.category,
+        manufacturer: component.manufacturer,
+      });
+    });
 };
 
 // Handle ComputerPart delete on POST.
-exports.computerpart_delete_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: ComputerPart delete POST");
+exports.computerpart_delete_post = function (req, res, next) {
+  ComputerPart.findByIdAndRemove(req.body.id, function deleteComponent(err) {
+    if (err) return next(err);
+
+    res.redirect("/components");
+  });
 };
 
 // Display ComputerPart update form on GET.
