5fc6b872e56bfa16d3306eda36530d8452aedcff
Sher <sher.sheikh77@gmail.com>
2020-11-08 17:39:35 -0500

    1. Add base styling to error page
    2. Position footer at bottom of page

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use('/modules', express.static(__dirname + '/node_modules/'));
+app.use('/fontawesome', express.static(__dirname + '/node_modules/@fortawesome/fontawesome-free/'));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
@@ -50,7 +50,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render("error", {title: "Oops... There was an error"});
 });
 
 module.exports = app;
