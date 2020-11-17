e907813b08298c7a188d9ae37a8a2f7b59bb2f39
Sher <sher.sheikh77@gmail.com>
2020-11-16 21:53:26 -0500

    Add frown icon when search result is empty

--- public/javascripts/searchFilter.js
+++ public/javascripts/searchFilter.js
@@ -1,14 +1,21 @@
-const numProds = document.getElementById('numProducts');
-
+const numProds = document.getElementById("numProducts");
+document.getElementById("filterForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+});
 document.getElementById("filter").addEventListener("input", (e) => {
   const filter = e.target.value.toUpperCase();
   const trs = document.getElementsByClassName("part-row");
   for (const tr of trs) {
-    if(tr.dataset.part.toUpperCase().includes(filter)) {
-      tr.style.display = ''
+    if (tr.dataset.part.toUpperCase().includes(filter)) {
+      tr.style.display = "";
     } else {
-      tr.style.display = 'none'
+      tr.style.display = "none";
     }
   }
-  numProds.textContent = Array.from(trs).filter(tr => tr.style.display == '').length;
+  let num = Array.from(trs).filter((tr) => tr.style.display == "").length;
+  numProds.textContent = num;
+
+  num == 0
+    ? document.getElementById("notFound").classList.remove("hidden")
+    : document.getElementById("notFound").classList.add("hidden");
 });
