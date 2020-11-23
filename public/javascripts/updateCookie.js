da122abbdd26152a657be80f3c69b551ca664a70
Sher <sher.sheikh77@gmail.com>
2020-11-23 17:29:40 -0500

    Change cookie key value pair

--- public/javascripts/updateCookie.js
+++ public/javascripts/updateCookie.js
@@ -2,17 +2,10 @@ const addButtons = document.getElementsByClassName("add");
 
 for (const button of addButtons) {
   button.addEventListener("click", () => {
-    const info = JSON.parse(button.dataset.info);
-    const url = button.dataset.url;
-    const key = info.category._id;
-    const formatted_obj = {
-      component: info.name,
-      component_url: url,
-      price: info.price,
-    };
-    document.cookie = `${key}=${JSON.stringify(
-      formatted_obj
-    )}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/; SameSite=None;`;
+    console.log(button.dataset.partid)
+    const partID = button.dataset.partid;
+    const key = button.dataset.categoryid;
+    document.cookie = `${key}=${partID}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/; SameSite=None;`;
     window.location.replace("/");
   });
 }
